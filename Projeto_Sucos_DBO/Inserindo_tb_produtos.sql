f2069c3c10dcd3c06e9e4fc367ade7ac7f285159
githubdojesseduardo <github_jesseduardo@outlook.com>
2024-10-10 18:06:49 -0300

    Rename Inserindo_tb_produtos.sql to Projeto_Sucos_DBO/Inserindo_tb_produtos.sql

--- Projeto_Sucos_DBO/Inserindo_tb_produtos.sql
+++ Projeto_Sucos_DBO/Inserindo_tb_produtos.sql
@@ -1,71 +1,71 @@
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('1040107', 'Light - 350 ml - Mel‚ncia', 'Lata', '350 ml', 'Mel‚ncia', 4.56);
+('1040107', 'Light - 350 ml - Mel√¢ncia', 'Lata', '350 ml', 'Mel√¢ncia', 4.56);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
 ('1037797', 'Clean - 2 Litros - Laranja', 'PET', '2 Litros', 'Laranja', 16.01);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
 ('1000889', 'Sabor da Montanha - 700 ml - Uva', 'Garrafa', '700 ml', 'Uva', 6.31);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('1004327', 'Videira do Campo - 1,5 Litros - Mel‚ncia', 'PET', '1,5 Litros', 'Mel‚ncia', 19.51);
+('1004327', 'Videira do Campo - 1,5 Litros - Mel√¢ncia', 'PET', '1,5 Litros', 'Mel√¢ncia', 19.51);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('1088126', 'Linha Citros - 1 Litro - Lim„o', 'PET', '1 Litro', 'Lim„o', 7.00);
+('1088126', 'Linha Citros - 1 Litro - Lim√£o', 'PET', '1 Litro', 'Lim√£o', 7.00);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('544931', 'Frescor do Ver„o - 350 ml - Lim„o', 'Lata', '350 ml', 'Lim„o', 2.46);
+('544931', 'Frescor do Ver√£o - 350 ml - Lim√£o', 'Lata', '350 ml', 'Lim√£o', 2.46);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('1078680', 'Frescor do Ver„o - 470 ml - Manga', 'Garrafa', '470 ml', 'Manga', 5.18);
+('1078680', 'Frescor do Ver√£o - 470 ml - Manga', 'Garrafa', '470 ml', 'Manga', 5.18);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('1042712', 'Linha Citros - 700 ml - Lim„o', 'Garrafa', '700 ml', 'Lim„o', 4.90);
+('1042712', 'Linha Citros - 700 ml - Lim√£o', 'Garrafa', '700 ml', 'Lim√£o', 4.90);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('788975', 'PedaÁos de Frutas - 1,5 Litros - MaÁa', 'PET', '1,5 Litros', 'MaÁa', 18.01);
+('788975', 'Peda√ßos de Frutas - 1,5 Litros - Ma√ßa', 'PET', '1,5 Litros', 'Ma√ßa', 18.01);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('1002767', 'Videira do Campo - 700 ml - Cereja/MaÁa', 'Garrafa', '700 ml', 'Cereja/MaÁa', 8.41);
+('1002767', 'Videira do Campo - 700 ml - Cereja/Ma√ßa', 'Garrafa', '700 ml', 'Cereja/Ma√ßa', 8.41);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('231776', 'Festival de Sabores - 700 ml - AÁai', 'Garrafa', '700 ml', 'AÁai', 13.31);
+('231776', 'Festival de Sabores - 700 ml - A√ßai', 'Garrafa', '700 ml', 'A√ßai', 13.31);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
 ('479745', 'Clean - 470 ml - Laranja', 'Garrafa', '470 ml', 'Laranja', 3.77);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('1051518', 'Frescor do Ver„o - 470 ml - Lim„o', 'Garrafa', '470 ml', 'Lim„o', 3.30);
+('1051518', 'Frescor do Ver√£o - 470 ml - Lim√£o', 'Garrafa', '470 ml', 'Lim√£o', 3.30);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('1101035', 'Linha Refrescante - 1 Litro - Morango/Lim„o', 'PET', '1 Litro', 'Morango/Lim„o', 9.01);
+('1101035', 'Linha Refrescante - 1 Litro - Morango/Lim√£o', 'PET', '1 Litro', 'Morango/Lim√£o', 9.01);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('229900', 'PedaÁos de Frutas - 350 ml - MaÁa', 'Lata', '350 ml', 'MaÁa', 4.21);
+('229900', 'Peda√ßos de Frutas - 350 ml - Ma√ßa', 'Lata', '350 ml', 'Ma√ßa', 4.21);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
 ('1086543', 'Linha Refrescante - 1 Litro - Manga', 'PET', '1 Litro', 'Manga', 11.01);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('695594', 'Festival de Sabores - 1,5 Litros - AÁai', 'PET', '1,5 Litros', 'AÁai', 28.51);
+('695594', 'Festival de Sabores - 1,5 Litros - A√ßai', 'PET', '1,5 Litros', 'A√ßai', 28.51);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
 ('838819', 'Clean - 1,5 Litros - Laranja', 'PET', '1,5 Litros', 'Laranja', 12.01);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
 ('326779', 'Linha Refrescante - 1,5 Litros - Manga', 'PET', '1,5 Litros', 'Manga', 16.51);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('520380', 'PedaÁos de Frutas - 1 Litro - MaÁa', 'PET', '1 Litro', 'MaÁa', 12.01);
+('520380', 'Peda√ßos de Frutas - 1 Litro - Ma√ßa', 'PET', '1 Litro', 'Ma√ßa', 12.01);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('1041119', 'Linha Citros - 700 ml - Lima/Lim„o', 'Garrafa', '700 ml', 'Lima/Lim„o', 4.90);
+('1041119', 'Linha Citros - 700 ml - Lima/Lim√£o', 'Garrafa', '700 ml', 'Lima/Lim√£o', 4.90);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('243083', 'Festival de Sabores - 1,5 Litros - Maracuj·', 'PET', '1,5 Litros', 'Maracuj·', 10.51);
+('243083', 'Festival de Sabores - 1,5 Litros - Maracuj√°', 'PET', '1,5 Litros', 'Maracuj√°', 10.51);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
 ('394479', 'Sabor da Montanha - 700 ml - Cereja', 'Garrafa', '700 ml', 'Cereja', 8.41);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('746596', 'Light - 1,5 Litros - Mel‚ncia', 'PET', '1,5 Litros', 'Mel‚ncia', 19.51);
+('746596', 'Light - 1,5 Litros - Mel√¢ncia', 'PET', '1,5 Litros', 'Mel√¢ncia', 19.51);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
 ('773912', 'Clean - 1 Litro - Laranja', 'PET', '1 Litro', 'Laranja', 8.01);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('826490', 'Linha Refrescante - 700 ml - Morango/Lim„o', 'Garrafa', '700 ml', 'Morango/Lim„o', 6.31);
+('826490', 'Linha Refrescante - 700 ml - Morango/Lim√£o', 'Garrafa', '700 ml', 'Morango/Lim√£o', 6.31);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('723457', 'Festival de Sabores - 700 ml - Maracuj·', 'Garrafa', '700 ml', 'Maracuj·', 4.91);
+('723457', 'Festival de Sabores - 700 ml - Maracuj√°', 'Garrafa', '700 ml', 'Maracuj√°', 4.91);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
 ('812829', 'Clean - 350 ml - Laranja', 'Lata', '350 ml', 'Laranja', 2.81);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('290478', 'Videira do Campo - 350 ml - Mel‚ncia', 'Lata', '350 ml', 'Mel‚ncia', 4.56);
+('290478', 'Videira do Campo - 350 ml - Mel√¢ncia', 'Lata', '350 ml', 'Mel√¢ncia', 4.56);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
 ('783663', 'Sabor da Montanha - 700 ml - Morango', 'Garrafa', '700 ml', 'Morango', 7.71);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('235653', 'Frescor do Ver„o - 350 ml - Manga', 'Lata', '350 ml', 'Manga', 3.86);
+('235653', 'Frescor do Ver√£o - 350 ml - Manga', 'Lata', '350 ml', 'Manga', 3.86);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('1002334', 'Linha Citros - 1 Litro - Lima/Lim„o', 'PET', '1 Litro', 'Lima/Lim„o', 7.00);
+('1002334', 'Linha Citros - 1 Litro - Lima/Lim√£o', 'PET', '1 Litro', 'Lima/Lim√£o', 7.00);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('1013793', 'Videira do Campo - 2 Litros - Cereja/MaÁa', 'PET', '2 Litros', 'Cereja/MaÁa', 24.01);
+('1013793', 'Videira do Campo - 2 Litros - Cereja/Ma√ßa', 'PET', '2 Litros', 'Cereja/Ma√ßa', 24.01);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
 ('1096818', 'Linha Refrescante - 700 ml - Manga', 'Garrafa', '700 ml', 'Manga', 7.71);
 INSERT INTO TB_PRODUTOS (PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) VALUES
-('1022450', 'Festival de Sabores - 2 Litros - AÁai', 'PET', '2 Litros', 'AÁai', 38.01);
+('1022450', 'Festival de Sabores - 2 Litros - A√ßai', 'PET', '2 Litros', 'A√ßai', 38.01);
 
